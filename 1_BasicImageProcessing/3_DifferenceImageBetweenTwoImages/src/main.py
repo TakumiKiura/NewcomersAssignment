3c10dd0e37b5e8127d314e5dee304892e291c20d
TakumiKiura <kiutaku0212@outlook.jp>
2026-04-21 05:12:18 +0900

    feat: add matplotlib and show the images

--- 1_BasicImageProcessing/3_DifferenceImageBetweenTwoImages/src/main.py
+++ 1_BasicImageProcessing/3_DifferenceImageBetweenTwoImages/src/main.py
@@ -1,36 +1,61 @@
 import cv2
+import matplotlib.pyplot as plt
 import os
 
 # set a relative path for using the image folder
 dir_path_src = os.path.dirname(__file__)
-dir_path_img = os.path.join(dir_path_src, "..", "img")
+dir_path_data = os.path.join(dir_path_src, "..", "data")
+dir_path_output = os.path.join(dir_path_src, "..", "output")
 
 # read images
-img_original = cv2.imread(os.path.join(dir_path_img, "original01.jpg"))
-cv2.imshow("original", img_original)
-cv2.waitKey(0)
-cv2.destroyWindow("original")
+img_original = cv2.imread(os.path.join(dir_path_data, "original01.jpg"))
 
-img_modified = cv2.imread(os.path.join(dir_path_img, "modified01.jpg"))
-cv2.imshow("modified", img_modified)
-cv2.waitKey(0)
-cv2.destroyWindow("modified")
+img_modified = cv2.imread(os.path.join(dir_path_data, "modified01.jpg"))
 
 # check the sizes of two images
 if img_original.shape != img_modified.shape:
     print("you need to resize the image.")
     exit()
 
-# get difference between two images
+# get difference image between two images
 diff = cv2.absdiff(img_original, img_modified)
 diff_gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("diff", diff)
-cv2.waitKey(0)
-cv2.destroyWindow("diff")
+# save as .jpg
+result_diff = cv2.imwrite(os.path.join(dir_path_output, "diff.jpg"), diff)
+if result_diff == False:
+    print("Can't save the image: diff.jpg")
+    exit()
 
-cv2.imshow("diff_gray", diff_gray)
-cv2.waitKey(0)
-cv2.destroyWindow("diff_gray")
+result_diff_gray = cv2.imwrite(os.path.join(dir_path_output, "diff_gray.jpg"), diff_gray)
+if result_diff_gray == False:
+    print("Can't save the image: diff_gray.jpg")
+    exit()
 
-cv2.destroyAllWindows()
+# change from BGR to RGB for matplotlib
+img_original_rgb = cv2.cvtColor(img_original, cv2.COLOR_BGR2RGB)
+img_modified_rgb = cv2.cvtColor(img_modified, cv2.COLOR_BGR2RGB)
+img_diff = cv2.cvtColor(diff, cv2.COLOR_BGR2RGB)
+img_diff_gray = cv2.cvtColor(diff_gray, cv2.COLOR_BGR2RGB)
+
+# output by matplotlib
+fig, axs = plt.subplots(2, 2, figsize=(10, 15))
+
+axs[0, 0].imshow(img_original_rgb)
+axs[0, 0].set_title("original")
+axs[0, 0].axis("off")
+
+axs[0, 1].imshow(img_modified_rgb)
+axs[0, 1].set_title("modified")
+axs[0, 1].axis("off")
+
+axs[1, 0].imshow(img_diff)
+axs[1, 0].set_title("difference(colored)")
+axs[1, 0].axis("off")
+
+axs[1, 1].imshow(img_diff_gray)
+axs[1, 1].set_title("difference(grayscale)")
+axs[1, 1].axis("off")
+
+plt.tight_layout()
+plt.show()
